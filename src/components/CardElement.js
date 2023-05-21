37c76c2452ac9db1d2bdffc644398f2eb9eb2468
Paulo Júnior <paulojrcosta300@gmail.com>
2023-05-21 09:22:51 -0300

    Creating card with React

---
+++ src/components/CardElement.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const CardElement = ({ card }) => {
+  return (
+    <div id={card.id} className="card">
+      <div className="card_front">
+        <img
+        className="icon"
+        src={`assets/images/${card.icon}.png`}
+        alt={card.icon}></img>
+      </div>
+      <div className="card_back">
+        {"</>"}
+      </div>
+    </div>
+  ) 
+}
+
+export default CardElement
